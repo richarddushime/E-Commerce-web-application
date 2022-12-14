004f1d320da7a97a476842f718d4a6bde5d69e90
DUSHIME MUDAHERA RICHARD <mudaherarich@gmail.com>
2022-12-14 17:31:49 +0300

    All images loaded

--- ecom/settings.py
+++ ecom/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
